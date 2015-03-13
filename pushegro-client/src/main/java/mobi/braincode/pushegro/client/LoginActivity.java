fa499ca81d26e898e06e91f70a8f823f9d2c354c
Krzysztof Slonka <slonka@student.agh.edu.pl>
2015-03-13 23:41:34 +0100

    Add two views

--- pushegro-client/src/main/java/mobi/braincode/pushegro/client/LoginActivity.java
+++ pushegro-client/src/main/java/mobi/braincode/pushegro/client/LoginActivity.java
@@ -11,7 +11,7 @@ public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_query_list);
     }
 
 
