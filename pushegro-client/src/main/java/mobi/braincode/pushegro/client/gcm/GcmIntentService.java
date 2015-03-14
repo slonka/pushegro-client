b04ac056e70e429375fd1eaa0300c0d4cfe04fad
Grzegorz Miejski <grz.miejski@gmail.com>
2015-03-14 14:55:46 +0100

    Changes in auctions message

--- pushegro-client/src/main/java/mobi/braincode/pushegro/client/gcm/GcmIntentService.java
+++ pushegro-client/src/main/java/mobi/braincode/pushegro/client/gcm/GcmIntentService.java
@@ -16,11 +16,14 @@ import mobi.braincode.pushegro.client.LoginActivity;
 import mobi.braincode.pushegro.client.QueryListActivity;
 import mobi.braincode.pushegro.client.R;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class GcmIntentService extends IntentService {
 
     public static final String SERVER_MESSAGE = "predicatesChanged";
+    public static final String SERVER_PREDICATES_NAMES = "predicatesNamesChanged";
     private AtomicInteger notificationId = new AtomicInteger();
 
     public GcmIntentService() {
@@ -32,6 +35,10 @@ public class GcmIntentService extends IntentService {
 
         Bundle extras = intent.getExtras();
         String predicatesChanged = extras.getCharSequence(SERVER_MESSAGE).toString();
+        String predicatesNamesChanged = extras.getCharSequence(SERVER_PREDICATES_NAMES).toString();
+
+        List<String> predicatesNames = Arrays.asList(predicatesNamesChanged.split(","));
+
         GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
         String messageType = gcm.getMessageType(intent);
 
@@ -41,11 +48,23 @@ public class GcmIntentService extends IntentService {
             } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)) {
                 handleGcmMessage("Deleted messages on server: " + extras.toString());
             } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
-                showNotification(getApplicationContext(), "Title", "notification from GCM", predicatesChanged);
+                showNotification(getApplicationContext(), "Pushegro wykryło zmiany!", generateNotificationMessage(predicatesNames), predicatesChanged);
                 handleGcmMessage(predicatesChanged);
             }
         }
         GcmBroadcastReceiver.completeWakefulIntent(intent);
+    }
+
+    private String generateNotificationMessage(List<String> predicatesNames) {
+        String result = "Zmiany w aukcjach:\n";
+        result += "- " + predicatesNames.get(0);
+        if (predicatesNames.size() > 1) {
+            result += "\n- " + predicatesNames.get(1);
+        } else {
+            result += "\n" + "...";
+        }
+
+        return result;
     }
 
     private void handleGcmMessage(final String msg) {
