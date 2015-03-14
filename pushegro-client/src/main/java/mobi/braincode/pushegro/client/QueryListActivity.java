6f86c3b6335d378b550b6fe5cfac8ec617aea56f
Krzysztof Slonka <slonka@student.agh.edu.pl>
2015-03-14 02:03:37 +0100

    Add search dialog

---
+++ pushegro-client/src/main/java/mobi/braincode/pushegro/client/QueryListActivity.java
@@ -0,0 +1,78 @@
+package mobi.braincode.pushegro.client;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import com.software.shell.fab.ActionButton;
+
+
+public class QueryListActivity extends ActionBarActivity {
+
+    ActionButton addButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_query_list);
+        addButton = (ActionButton) findViewById(R.id.query_list_add_query);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Set up the input
+                final EditText input = new EditText(v.getContext());
+
+                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                input.setPadding(20,0,20,20);
+                input.setHint("Tytuł aukcji");
+
+                new AlertDialog.Builder(v.getContext())
+                    .setTitle("Wyszukiwanie")
+                        // Specify the list array, the items to be selected by default (null for none),
+                            // and the listener through which to receive callbacks when items are selected
+                        .setView(input)
+                                // Set the action buttons
+                        .setPositiveButton("Szukaj", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                            // User clicked OK, so save the mSelectedItems results somewhere
+                            // or return them to the component that opened the dialog
+                        }
+                    })
+                    .setNegativeButton("Anuluj", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+
+                        }
+                    })
+                    .show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
